0efa7ce93774fb1c0d14c9cf69335a7c0b6d2c51
LiGoAl <glalexei@mail.ru>
2026-08-19 15:20:50 +0300

    sixth commit

--- src/main/java/com/example/test/plug/Services/Delay.java
+++ src/main/java/com/example/test/plug/Services/Delay.java
@@ -2,7 +2,6 @@ package com.example.test.plug.Services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +13,6 @@ public class Delay {
     private final long highBorder;
     private final Logger logger = LoggerFactory.getLogger(Delay.class);
 
-    @Autowired
     public Delay(@Value("${app.delay.low_border}") long lowBorder, @Value("${app.delay.high_border}") long highBorder) {
         this.lowBorder = lowBorder;
         this.highBorder = highBorder;
